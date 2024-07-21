98ce17d5b7398e53073e3515d9b9768a1b3e8989
Antonio Llamas <antonio@allamas.com>
2024-07-21 18:39:40 +0200

    feat: add Ingress config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,4 +5,9 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => {
+
+    res.json({message: "Rafa"})
+})
+
 app.listen(PORT, () => console.log("Listening on 3000"))
