21afdced06e46a067482d7e21226c8de36d1a614
Antonio Llamas <antonio@allamas.com>
2024-07-22 14:27:09 +0200

    feat: add mongoose for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,19 @@
+import mongoose from 'mongoose'
 import { app } from './app'
 
 const PORT = 3000
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('MongoDB connected')
+
+    app.listen(PORT, () => {
+      console.log(`Listening on ${PORT}`)
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+start()
