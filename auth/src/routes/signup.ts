5c1f73182496c7f9242e2795f95d99d72e83b0f1
Antonio Llamas <antonio@allamas.com>
2024-07-22 08:39:00 +0200

    feat: add express-validator for inputs

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/signup', (req, res) => {
-  res.json({ message: 'Register' })
-})
+router.post(
+  '/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 16 })
+      .withMessage('Password must be between 4 and 16 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
+
+    const { email, password } = req.body
+
+    res.json({ message: 'Register' })
+  },
+)
 
 export { router as signupRouter }
