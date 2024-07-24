73840acfdd60c6306bb12aada95435a3734b21f8
Antonio Llamas <antonio@allamas.com>
2024-07-24 12:04:43 +0200

    refactor: extract validation logic for reuse

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import { User } from '../models/user'
-import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
@@ -15,10 +15,8 @@ router.post(
       .isLength({ min: 4, max: 16 })
       .withMessage('Password must be between 4 and 16 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) throw new RequestValidationError(errors.array())
-
     const { email, password } = req.body
     const existingUser = await User.findOne({ email })
 
